bfdbf807386f22fd673d793e1325197071d46ee1
Rav3nFK <shabanin.serzh@mail.ru>
2021-07-05 09:44:18 +0300

    Создание нового класса + метод считывания с файла

--- МДК 02. Критический/Program.cs
+++ МДК 02. Критический/Program.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.IO;
+using System.Diagnostics;
 
 namespace МДК_02.Критический
 {
@@ -10,6 +12,14 @@ namespace МДК_02.Критический
     {
         static void Main(string[] args)
         {
+            Debug.Listeners.Add(new TextWriterTraceListener(File.Create("LogOFCrit.txt")));
+            Debug.AutoFlush = true;
+
+            oKrit krit = new oKrit();
+            krit.Reshenie();
+
+
+
         }
     }
 }
---
+++ МДК 02. Критический/oKrit.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace МДК_02.Критический
+{
+    class oKrit
+    {
+        string str = "";
+        public struct ofPP
+        {
+            public int punkt1;
+            public int punkt2;
+            public int dlina;
+
+        }
+
+        public void Reshenie()
+        {
+            Vvod();
+
+        }
+
+        //!check
+        /// <summary>
+        /// Метод считывания из файла
+        /// </summary>
+        /// <returns> Возвращает заполненный лист</returns>
+        public List<ofPP> Vvod()
+        {
+            List<ofPP> l = new List<ofPP>();
+            using (StreamReader sr = new StreamReader("Ввод.csv"))
+            {
+                while (sr.EndOfStream != true)
+                {
+                    string[] count1 = sr.ReadLine().Split(';');
+                    string[] count2 = count1[0].Split('-');
+                    Debug.WriteLine(count2[0] + " - " + count2[1] + "; " + count1[1]);
+                    l.Add(new ofPP { punkt1 = Convert.ToInt32(count2[0]), punkt2 = Convert.ToInt32(count2[1]), dlina = Convert.ToInt32(count1[1]) });
+                }
+
+            }
+            return l;
+        }
+
+
+
+
+
+
+    }
+}
